dcd3b44da33191a128b6420bdd542b1218ed2ac4
speedskater1610 <speedskater1610@gmail.com>
2025-12-19 11:24:41 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ use crate::bot::Bot;
 #[vexide::main]
 async fn main(_peripherals: Peripherals) {
     let bot = Bot {};
-    bot.compete().await; // start the auton
+    bot.compete().await; // start the auton then go to driver controll.
+    sleep(vexide::adi::ADI_UPDATE_INTERVAL).await;
 }
