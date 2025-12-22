0fe78ea852f455cd9077b3a1191bd9f10f8baf78
speedskater1610 <speedskater1610@gmail.com>
2025-12-22 08:51:06 -0500

    Update main.rs: outline the bot

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,34 @@ struct Robot {
     // drive train
     left_motors: [Motor; 3],
     right_motors: [Motor; 3],
+
+    // bot io
+    in_take: Motor,
+    out_take: Motor,
+
+    // solenoids (pneumatics)
+    match_loader: AdiDigitalOut,
+    match_loader_state: bool,
+    output_ramp: AdiDigitalOut,
+    output_ramp_state: bool,
+}
+
+
+impl Robot {
+    async fn toggle_piston_a(&mut self) {
+        self.match_loader_state = !match_loader_state;
+        _ = self.match_loader.toggle();
+    }
+
+    async fn toggle_piston_b(&mut self) {
+        self.output_ramp_state = !output_ramp_state;
+        _ = self.output_ramp.toggle();
+    }
 }
 
 impl Compete for Robot {
-    async fn driver(&mut self) {
+    async fn driver(&mut self) { 
+        // TODO: print that I am tiking it in all caps (good luck);
         loop {
             let controller_state = self
                                     .controller
@@ -29,6 +53,10 @@ impl Compete for Robot {
             for motor in self.right_motors.iter_mut() {
                 _ = motor.set_voltage((forward - turn) * Motor::V5_MAX_VOLTAGE);
             }
+            
+            if controller_state.button_x.is_now_pressed() {
+                self.toggle_piston_a();
+            }
 
             sleep(Controller::UPDATE_INTERVAL).await;
         }
@@ -40,15 +68,22 @@ async fn main(peripherals: Peripherals) {
     Robot {
         controller: peripherals.primary_controller,
         left_motors: [
-            Motor::new(peripherals.port_1, Gearset::Blue, Direction::Reverse),
-            Motor::new(peripherals.port_2, Gearset::Blue, Direction::Reverse),
-            Motor::new(peripherals.port_3, Gearset::Blue, Direction::Forward),
+            Motor::new(peripherals.port_16, Gearset::Blue, Direction::Forward),
+            Motor::new(peripherals.port_18, Gearset::Blue, Direction::Forward),
+            Motor::new(peripherals.port_17, Gearset::Blue, Direction::Forward),
         ],
         right_motors: [
-            Motor::new(peripherals.port_4, Gearset::Blue, Direction::Forward),
-            Motor::new(peripherals.port_5, Gearset::Blue, Direction::Forward),
-            Motor::new(peripherals.port_6, Gearset::Blue, Direction::Reverse),
+            Motor::new(peripherals.port_13, Gearset::Blue, Direction::Reverse),
+            Motor::new(peripherals.port_15, Gearset::Blue, Direction::Reverse),
+            Motor::new(peripherals.port_15, Gearset::Blue, Direction::Reverse),
         ],
+        in_take: Motor::new(peripherals.port_20, Gearset::Blue, Direction::Forward),
+        out_take: Motor::new(peripherals.port_10, Gearset::Green, Direction::Forward),
+        match_loader: AdiDigitalOut::new(peripherals.adi_a),
+        output_ramp: AdiDigitalOut::new(peripherals.adi_b),
+        // Off by default.
+        match_loader_state: false,  
+        output_ramp_state: false,
     }
     .compete()
     .await;
